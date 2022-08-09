6826d38474c6ab4cb08003bc070c475edea87555
Josh Hawk <hawkjosh@icloud.com>
2022-08-08 20:53:07 -0400

    Content update push, also restructured helper code and messed around with logic until it started working

--- routes/notes.js
+++ routes/notes.js
@@ -1,64 +1,44 @@
 const notes = require('express').Router();
+const { readNote, writeNote, updateNote } = require('../helpers/fsUtilities.js');
 const { v4: uuidv4 } = require('uuid');
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require('../');
 
-// GET Route for retrieving all the notes
+// Route for retrieving all notes
 notes.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  readNote('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// GET Route for a specific note
-notes.get('./:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No note with that ID');
-    });
+// Route for retrieving specific note
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readNote('./db/db.json').then((data) => JSON.parse(data)).then((json) => {
+    const result = json.filter((note) => note.id === noteId);
+    return result.length > 0 ? res.json(result) : res.json('No note with that ID');
+  });
 });
 
-// DELETE Route for a specific note
-notes.delete('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
-
-      // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
-
-      // Respond to the DELETE request
-      res.json(`Item ${noteId} has been deleted 🗑️`);
-    });
+// Route for deleting specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readNote('./db/db.json').then((data) => JSON.parse(data)).then((json) => {
+    const result = json.filter((note) => note.id !== noteId);
+    writeNote('./db/db.json', result);
+    res.json(`Item ${noteId} has been deleted`);
+  });
 });
 
-// POST Route for a new UX/UI note
+// Route for posting new note
 notes.post('/', (req, res) => {
-  console.log(req.body);
-
   const { title, text } = req.body;
-
   if (req.body) {
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4()
     };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error('Error in adding note');
-  }
+    const noteId = req.params.id;
+    updateNote(newNote, './db/db.json');
+    res.json(`Note ${noteId} has been posted`);
+  } 
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-
 app.use(express.static('public'));
 
-// GET Route for homepage
+// Get route for landing page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
+// Get route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening at localhost:${PORT}`));
